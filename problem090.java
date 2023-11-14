59f6c7ed67f81f6afec11875556360193f7612ef
djgns0174 <74578132+djgns0174@users.noreply.github.com>
2023-11-14 18:40:42 +0900

    Update problem090.java

--- problem090.java
+++ problem090.java
@@ -1,7 +1,7 @@
 package problem100;
 import java.util.Scanner;
 
-public class ex90 {
+public class problem090 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
