3b12afbb4a11c0fc7eb3b97f1aac68535a2880b9
djgns0174 <74578132+djgns0174@users.noreply.github.com>
2023-11-14 18:38:47 +0900

    Update problem041.java

--- problem041.java
+++ problem041.java
@@ -1,7 +1,7 @@
 package problem100;
 import java.util.Scanner;
 
-public class ex41 {
+public class problem041 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
